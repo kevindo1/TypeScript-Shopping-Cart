64d12b72205e16facce0c2fa3ca862eabf08b728
Kevin Do <kevind0501@gmail.com>
2022-06-25 12:05:29 -0700

    item components

---
+++ src/Item/Item.tsx
@@ -0,0 +1,25 @@
+import Button from "@material-ui/core/Button";
+// TypeScript types
+import { CartItemType } from "../App";
+// Styles import
+import { Wrapper } from "../App.styles";
+
+type Props = {
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+};
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+  <Wrapper>
+    <img src={item.image} alt={item.title} />
+    <div>
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+      <h3>${item.price}</h3>
+    </div>
+    {/* in line arrow function so that the prop can be sent in, or else it will be triggered in right away */}
+    <Button onClick={() => handleAddToCart(item)}>Add to Cart</Button>
+  </Wrapper>
+);
+
+export default Item;
